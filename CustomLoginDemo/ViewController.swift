87e20f6d570bfd78dca4eddccb284e5197ed1ef5
Manas Duishoev <manasduishoev034@gmail.com>
2023-02-06 17:10:48 +0600

    Initial Commit

---
+++ CustomLoginDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomLoginDemo
+//
+//  Created by new on 6/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
